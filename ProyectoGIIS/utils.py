3cbec7d2628bd99c01e912812097d3ff23f32d75
Yexi Cortez <yexili92@gmail.com>
2022-08-15 17:54:50 +0200

    Crud base de datos

--- ProyectoGIIS/utils.py
+++ ProyectoGIIS/utils.py
@@ -1,11 +1,13 @@
+import pymongo
 from pymongo import MongoClient
-def get_db_handle(db_name, host, port, username, password):
 
- client = MongoClient(host=host,
-                      port=int(port),
-                      username=username,
-                      password=password
-                     )
- db_handle = client['db_name']
- return db_handle, client
- 
+conn_str ="mongodb+srv://'Proyecto GIIS':123$5678@db-giis.dfbdpjx.mongodb.net/?retryWrites=true&w=majority"
+try:
+    client = pymongo.MongoClient('conn_str')
+except Exception:
+    print("Error: " + Exception)
+
+
+db = client['DB-GIIS']
+
+print(client.list_database_names())
