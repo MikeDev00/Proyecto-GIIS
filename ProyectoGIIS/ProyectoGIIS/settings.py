318c6181440a57397d99662bd23ae85474544c7a
Yexi Cortez <yexili92@gmail.com>
2022-10-06 16:19:27 -0500

    Arreglo de color de alertas

--- ProyectoGIIS/ProyectoGIIS/settings.py
+++ ProyectoGIIS/ProyectoGIIS/settings.py
@@ -157,3 +157,11 @@ MEDIA_URL='/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
